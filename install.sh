7d80be2ecfbbcdcd7644b26d4ad60a5a2226c88d
jerrygems <shubham.debian@gmail.com>
2024-08-12 21:29:59 -0700

    updated install.sh

--- install.sh
+++ install.sh
@@ -32,20 +32,27 @@ genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt << EOF
 echo ${hostname} >> /etc/hostname
 
-usermod -a -G wheel -s /bin/bash ${username}
+useradd -a -G wheel -s /bin/bash ${username}
 echo "${uname}:${userpass}" | chpasswd
 
-mkinitcpio -P
-echo "root:${password}" | chpasswd
 
-pacman -Syu --noconfirm hyprland waybar wofi swaybg swaylock alacritty grim pulseaudio pulseaudio-ctl pavucontrol bluez bluez-utils networkmanager
+
+
+pacman -Syu --noconfirm hyprland waybar wofi swaybg swaylock alacritty grim pulseaudio pulseaudio-ctl pavucontrol bluez bluez-utils networkmanager grub os-prober
 systemctl restart NetworkManager
 systemctl restart pulseaudio.service
+systemctl restart bluetooth
+
+echo "root:${password}" | chpasswd
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
 exit
 EOF
 
 umount -R /mnt
-reboot
+# reboot
 
 
 
