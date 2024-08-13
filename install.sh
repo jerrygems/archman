48a49c0ae06470e565b8d887905695ba8e53476a
jerrygems <shubham.debian@gmail.com>
2024-08-12 20:47:12 -0700

    updated the install.sh

---
+++ install.sh
@@ -0,0 +1,56 @@
+read -p "enter the disk on which to apply: " disk
+read -p "enter the hostname for the setup: " hostname
+read -p "enter the password for root: " password
+read -p "enter the new user's name: " uname
+read -p "password for the new user: " userpass
+umount ${disk} 2>/dev/null
+parted ${disk} mklabel gpt # clearing partition
+parted ${disk} mkpart primary fat32 1MB 513MB # creating EFI
+parted ${disk} set 1 esp on # assigning the EFI flag here
+
+parted ${disk} mkpart primary ext4 513MB 210513MB # creating root partition
+parted ${disk} mkpart primary linux-swap 210513MB 215513 # creating the swap partition here
+
+partprobe ${disk} # inform the OS about changed partition table
+
+#formatting partitions
+mkfs.fat -F32 ${disk}1
+mkfs.ext4 ${disk}2
+
+# mount the root partition 
+mount ${disk}2 /mnt/
+mkdir -p /mnt/boot/
+mount ${disk}1 /mnt/boot/
+swapon ${disk}3
+
+pacstrap -K /mnt base linux linux-firmware
+
+# creating the table and saving inside the /mnt/etc/fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+
+arch-chroot /mnt << EOF
+echo ${hostname} >> /etc/hostname
+
+usermod -a -G wheel -s /bin/bash ${username}
+echo "${uname}:${userpass}" | chpasswd
+
+mkinitcpio -P
+echo "root:${password}" | chpasswd
+
+pacman -Syu --noconfirm hyprland waybar wofi swaybg swaylock alacritty grim pulseaudio pulseaudio-ctl pavucontrol bluez bluez-utils networkmanager
+systemctl restart NetworkManager
+systemctl restart pulseaudio.service
+exit
+EOF
+
+umount -R /mnt
+reboot
+
+
+
+
+
+
+
+
