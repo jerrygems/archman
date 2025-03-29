fbcdcc937f107c81725261006d5b64d8e9e95bc4
jerrygems <shubham.debian@gmail.com>
2025-03-29 20:18:13 +0530

    fixes for mount issue

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-umount -R /mnt
 read -p "enter the disk on which to apply(e.g. /dev/sdx): " disk
 read -p "enter the efi partition size(e.g. 512 Integer):" efi_size
 read -p "enter the swap partition size(e.g. 4096 Integer)" swap_size
@@ -6,6 +5,9 @@ read -p "enter the hostname for the setup(e.g. anyname): " hostname
 read -p "enter the password for root(e.g. @p455w0rd123): " password
 read -p "enter the new user's name: " username
 read -p "password for the new user: " userpass
+
+umount -R /mnt || true
+umount -R "${disk}"* 2>/dev/null || true
 
 if ! [[ -b "$disk" ]]; then
   echo "Specified invalid disk. Please specify correct disk.";
